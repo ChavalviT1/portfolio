b9763098e4ccec3321bfa4777fd38598e1a2707e
Chavalvit <s6552d10012@sau.ac.th>
2023-04-06 02:53:18 +0700

    commit

---
+++ sendEmail.php
@@ -0,0 +1,41 @@
+<?php
+    use PHPMailer\PHPMailer\PHPMailer;\
+
+    if (isset(S_POST['name']) &6 isset(S_POST['email'])){
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+        
+        require_once "PHPMailer/PHPMailer.php";
+        require_once "PHPMailer/SMTP.php" ;
+        require_once "PHPMailer/Exception.php";
+        
+        $mail = new PHPMailer();
+        
+        $mail->isSMTP();
+        $mail->Host = "smtp.gmail.com";
+        $mail->SMTPAuth = true;
+        $mail->Username = "chavalvit37@gmail.com";
+        $mail->Password = "Chavalvit1";
+        $mail->Port = 465;
+        $mail->SMTPSecure = "ssl";
+
+        $mail->isHTML(true);
+        $mail->setfrom($email, $name);
+        $mail->addAddress($email);
+        $mail->Body = $message;
+
+        if($mail->send()) {
+            $status = "success";
+            $response = "Email is sent";
+        } else {
+            $status = "failed";
+            $response = "Something is wrong" . $mail->ErrorInfo;
+        }
+        
+        exit(json_encode(array("status" => $status, "response" => $response)));
+    }    
+
+
+
+?>
